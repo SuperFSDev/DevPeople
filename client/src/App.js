001f5a64182e1a2e0d0b0d40615f94e25b32cd01
SuperFSDev <Krishi119@outlook.com>
2020-10-30 20:22:14 +0530

    set up routers

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,26 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import login from './components/auth/Login';
+import register from './components/auth/Register';
 
 const App = () => {
   return (
-    <Fragment>
-      <Navbar />
-      <Landing />
-    </Fragment>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <div className="container">
+          <Switch>
+            <Route exact path="/register" component={register} />
+            <Route exact path="/login" component={login} />
+          </Switch>
+        </div>
+      </Fragment>
+    </Router>
   );
 };
 
