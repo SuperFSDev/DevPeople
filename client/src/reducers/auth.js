18880918137e93a2353025c18e9aaef87e8a703f
SuperFSDev <Krishi119@outlook.com>
2020-11-05 19:31:02 +0530

    navbar links

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -6,6 +6,7 @@ import {
   AUTH_ERROR,
   LOGIN_FAIL,
   LOGIN_SUCCESS,
+  LOGOUT,
 } from '../actions/types';
 //import { setAlert } from '../actions/alert';
 
@@ -39,6 +40,7 @@ export default function (state = initialState, action) {
     case REGISTER_FAIL:
     case AUTH_ERROR:
     case LOGIN_FAIL:
+    case LOGOUT:
       localStorage.removeItem('token');
       return {
         ...state,
