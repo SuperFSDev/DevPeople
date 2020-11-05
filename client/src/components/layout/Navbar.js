8e1f04ccb76c0c20bc6bf89333633e5fc04e2a66
SuperFSDev <Krishi119@outlook.com>
2020-11-05 21:01:04 +0530

    fix navbar login logout

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { connect } from 'mongoose';
+import { connect } from 'react-redux';
 
 import { logout } from '../../actions/auth';
 
